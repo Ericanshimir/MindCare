b19d0bcd0e4779a9c3fc66c1e8698748e451a6d5
Erica Nshimirimana <e.nshimirim@alustudent.com>
2025-03-07 21:38:36 +0300

    file

--- MindCare/eyt/settings.py
+++ MindCare/eyt/settings.py
@@ -93,10 +93,15 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eyt',
+        'USER':'postgres',
+        'PASSWORD': 'acire',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
